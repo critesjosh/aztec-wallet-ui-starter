48268c12493fd59ae57ff123329127ddead016c7
josh crites <jc@joshcrites.com>
2024-08-28 14:36:06 -0400

    Update vite.config.ts

--- my-vite-app/vite.config.ts
+++ my-vite-app/vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import resolve from "vite-plugin-resolve";
+import fs from "fs";
+import path from "path";
 
-const aztecVersion = "0.51.0";
+const aztecVersion = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json")).toString())["dependencies"]["@aztec/aztec.js"];
+const cleanedVersion = aztecVersion.replace(/^\^/, '');
 
 export default defineConfig({
     plugins: [
         process.env.NODE_ENV === "production"
             ? /** @type {any} */ (
                 resolve({
-                    "@aztec/bb.js": `export * from "https://unpkg.com/@aztec/bb.js@${aztecVersion}/dest/browser/index.js"`,
+                    "@aztec/bb.js": `export * from "https://unpkg.com/@aztec/bb.js@${cleanedVersion}/dest/browser/index.js"`,
                 })
             )
             : undefined,
