6357ef9c8e55221346213561c43c8fa769e9b8fb
josh crites <jc@joshcrites.com>
2024-08-29 09:32:41 -0400

    update to 51 and deploy schnorr

--- my-vite-app/src/main.ts
+++ my-vite-app/src/main.ts
@@ -1,5 +1,7 @@
 import './style.css'
 import { AccountWallet, CompleteAddress, ContractDeployer, createDebugLogger, Fr, PXE, waitForPXE, TxStatus, createPXEClient, getContractInstanceFromDeployParams, DebugLogger } from "@aztec/aztec.js";
+import { getSchnorrAccount } from '@aztec/accounts/schnorr';
+import { deriveSigningKey } from '@aztec/circuits.js';
 
 
 const setupSandbox = async () => {
@@ -16,6 +18,27 @@ try {
   console.log(accounts)
   //let number = await pxe.getBlockNumber()
   console.log(await pxe.getBlockNumber())
+
+  const button = document.createElement('button');
+  button.innerText = 'Click me!';
+  button.addEventListener('click', async () => {
+    await deploySchnorrAccount(pxe);
+  });
+
+  document.body.appendChild(button);
 } catch (e) {
   console.log(e)
+}
+
+
+async function deploySchnorrAccount(pxe: PXE) {
+
+  let secretKey = Fr.random();
+  let salt = Fr.random();
+
+  let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
+  // const { address, publicKeys, partialAddress } = schnorrAccount.getCompleteAddress();
+  let wallet = await schnorrAccount.register();
+  let txReceipt = await schnorrAccount.deploy().getReceipt();
+  console.log(wallet, txReceipt);
 }
